c6aac70949d624e05735dc589d6751ae01da5e8a
robe <robe@robe.com>
2024-02-03 20:36:39 -0600

    Agregar archivos de proyecto.

---
+++ Bee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_IStingPatrol
+{
+    class Bee:IStingPatrol
+    {
+        private int alertLevel;
+        private int stingerLength;
+        public int AlertLevel
+        {
+            get
+            {
+                return alertLevel;
+            }
+        }
+        public int StingerLength
+        {
+            get
+            {
+                return stingerLength;
+            }
+            set
+            {
+                stingerLength = value;
+            }
+        }
+        public bool LookForEnemies()
+        {
+            return true;
+        }
+
+        public int SharpenStinger(int length)
+        {
+            return 0;
+        }
+    }
+}
